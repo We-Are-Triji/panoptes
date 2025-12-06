0f22c1be07ce58fba149f8775cbb860995142d18
Gauciv <vincentaugusto16@gmail.com>
2025-12-06 02:33:26 +0000

    chore: add entities to database for testing

---
+++ Panoptes.Core/Interfaces/IAppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Panoptes.Core.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Panoptes.Core.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<WebhookSubscription> WebhookSubscriptions { get; }
+        DbSet<DeliveryLog> DeliveryLogs { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Panoptes.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Panoptes.Core.Entities;
+using Panoptes.Core.Interfaces;
+
+namespace Panoptes.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WebhookSubscription> WebhookSubscriptions { get; set; }
+        public DbSet<DeliveryLog> DeliveryLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<WebhookSubscription>(entity =>
+            {
+                entity.Property(e => e.SecretKey)
+                    .IsRequired();
+
+                entity.Property(e => e.TargetUrl)
+                    .IsRequired();
+            });
+        }
+    }
+}
