e6d070102246959d8f1efb35575ac11658c6c709
Tyrone  Tabornal <53328093+Nesqyk@users.noreply.github.com>
2025-12-10 19:52:52 +0800

    chore(client): removed unused components

--- Panoptes.Client/src/pages/landing/sections/SystemTelemetry.tsx
+++ Panoptes.Client/src/pages/landing/sections/SystemTelemetry.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { motion} from 'framer-motion';
 import clsx from 'clsx';
-import { GlitchButton } from '@/components/GlitchButton';
 import { MaskText } from '@/components/MaskText';
 
 // --- MOCK STATS ---
