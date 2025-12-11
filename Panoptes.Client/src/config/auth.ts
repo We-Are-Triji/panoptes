fac6be726e7f69a3aa9140d6396fdb2485d6226b
Gauciv <vincentaugusto16@gmail.com>
2025-12-11 02:31:34 +0000

    feat: add google sign in option

--- Panoptes.Client/src/config/auth.ts
+++ Panoptes.Client/src/config/auth.ts
@@ -6,6 +6,15 @@ export const configureAuth = () => {
       Cognito: {
         userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
         userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+        loginWith: {
+          oauth: {
+            domain: import.meta.env.VITE_COGNITO_DOMAIN,
+            scopes: ['email', 'profile', 'openid'],
+            redirectSignIn: [import.meta.env.VITE_REDIRECT_URI],
+            redirectSignOut: [import.meta.env.VITE_REDIRECT_URI],
+            responseType: 'code'
+          }
+        }
       }
     }
   });
